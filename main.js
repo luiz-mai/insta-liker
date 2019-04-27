418c2e4dd0cd97cced2186216a5577103ccabd45
Luiz Mai <lffmai@gmail.com>
2019-04-27 19:01:30 -0300

    Searching for tags

--- main.js
+++ main.js
@@ -5,10 +5,17 @@ const puppeteer = require('puppeteer');
 (async () => {
     const email = process.env.FACEBOOK_EMAIL
     const password = process.env.FACEBOOK_PASSWORD
+    const photosToLike = process.env.N_PHOTOS_TO_LIKE
+    const tagToSearch = process.env.TAG_TO_SEARCH
+
+    const instagramHomeURL = "https://instagram.com"
+    const instagramExploreURL = "https://instagram.com/explore"
+    const instagramTagURL = "https://www.instagram.com/explore/tags/{tag}"
+
     const browser = await puppeteer.launch({ headless: false });
     //   const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://instagram.com');
+    await page.goto(instagramHomeURL);
     await page.waitFor("form[method=post] button")
     await page.click("form[method=post] button")
 
@@ -21,8 +28,23 @@ const puppeteer = require('puppeteer');
         page.waitForNavigation({ waitUntil: 'networkidle0' }),
     ]);
 
+    if(tagToSearch) {
+        await page.goto(instagramTagURL.replace("{tag}", tagToSearch))
+    } else {
+        await page.goto(instagramExploreURL)
+    }
+    await page.waitFor("article img")
+    await page.click("article img")
 
-    await page.goto('https://instagram.com/explore');
+    let counter = 0
+
+    for(counter = 0; counter < photosToLike; counter++){
+        await page.waitFor("span[aria-label=Curtir]")
+        await page.click("span[aria-label=Curtir]")
+        await page.waitFor(500)
+        await page.waitFor(".coreSpriteRightPaginationArrow")
+        await page.click(".coreSpriteRightPaginationArrow")
+    }
 
     await browser.close();
 })();
