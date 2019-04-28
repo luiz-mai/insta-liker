15bf262f6fbc5acdee3e0cc8a6f8a80db5bf0520
Luiz Mai <lffmai@gmail.com>
2019-04-27 21:08:02 -0300

    No sandbox arg

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ const puppeteer = require('puppeteer');
     const instagramExploreURL = "https://instagram.com/explore"
     const instagramTagURL = "https://www.instagram.com/explore/tags/{tag}"
 
-    // const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-      const browser = await puppeteer.launch( {headless: false});
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    //   const browser = await puppeteer.launch( {headless: false});
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36');
     await page.goto(instagramHomeURL);
